ba3f1cad206b1f538ce184ac50f9a0ec10129b76
Quinn Cheong <quinncheong@hotmail.com>
2023-10-09 19:24:57 +0800

    Remove unused imports

--- frontend/src/app/portfolios/page.js
+++ frontend/src/app/portfolios/page.js
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState, useEffect } from 'react'
+"use client";
+
 import { useGetPortfolios } from "@api/portfolio.js";
 
 export default function portfolio() {
@@ -8,9 +8,20 @@ export default function portfolio() {
   if (isLoading) return (<div>Loading...</div>)
   if (isError) return (<div>Error: {error.message}</div>)
 
+  const renderPortfolios = () => {
+    return data.map((portfolio, index) => {
+      return (
+        <div key={index}>
+          <div>{portfolio.name}</div>
+          <div>{portfolio.description}</div>
+        </div>
+      )
+    })
+  }
+
   return (
     <>
-      <div>This is the portfolio page</div>
+      <div>{renderPortfolios()}</div>
     </>
   )
 }
