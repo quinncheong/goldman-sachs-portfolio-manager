33fbebe62da1c31f4d1cd04545c0ce276b100395
Quinn Cheong <quinncheong@hotmail.com>
2023-10-09 09:30:37 +0800

    Add daisyUI and heroicons

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Box from "tailwindcss";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gs-blue-1">
+      div.
+    </main>
+  )
+}
