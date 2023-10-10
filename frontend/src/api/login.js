e019f7729b8b37217496814c5bece8d57410ade9
Quinn Cheong <quinncheong@hotmail.com>
2023-10-10 12:48:48 +0800

    Add loader and login component

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -3,36 +3,55 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { BASE_SERVER_URL, LOGIN_API_PATH } from "./apiFactory";
 
 const axiosLoginInstance = axios.create({
-  baseURL: BASE_SERVER_URL + PORTFOLIO_API_PATH,
+  baseURL: BASE_SERVER_URL + LOGIN_API_PATH,
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export const useLogin = () => {
-  const queryClient = useQueryClient();
-  const {
-    isLoading: isCreating,
-    isSuccess: isSuccessCreating,
-    isError: isErrorCreating,
-    error: errorCreateing,
-    mutate: CreateTodo,
-  } = useMutation((data) => postTodoFn(data), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["todos"]);
-    },
+export const useGetLoginStatus = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["getPortfolios"],
+    queryFn: () => getLoginStatus(),
+  });
+
+  return { data, isLoading, isError, error };
+};
+
+const getLoginStatus = async () => {
+  let isLoggedIn = false;
+  if (process.env.NODE_ENV === "production") {
+    let response = await axiosLoginInstance.get("/all");
+    isLoggedIn = response.data;
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(isLoggedIn);
+    }, 2000);
   });
 };
 
-const getPortfolios = async () => {
-  if (process.env.NODE_ENV !== "production") {
-    return mockPortfolios;
-  }
-  let response = await axiosInstance.get("/all");
-  return response.data;
-};
+// export const useLogin = () => {
+//   const queryClient = useQueryClient();
+//   const {
+//     isLoading: isCreating,
+//     isSuccess: isSuccessCreating,
+//     isError: isErrorCreating,
+//     error: errorCreateing,
+//     mutate: CreateTodo,
+//   } = useMutation((data) => postTodoFn(data), {
+//     onSuccess: () => {
+//       queryClient.invalidateQueries(["todos"]);
+//     },
+//   });
+// };
 
-export const getPortfolioByPortfoliId = (portfolioId) => {
-  return axiosInstance.get("/" + portfolioId);
-};
+// const getPortfolios = async () => {
+//   if (process.env.NODE_ENV !== "production") {
+//     return mockPortfolios;
+//   }
+//   let response = await axiosInstance.get("/all");
+//   return response.data;
+// };
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import Login from "@/components/login/Login";
+import { useGetLoginStatus } from "@/api/login";
+import Loader from "@/components/loading/Loader";
 
 export default function App({ children }) {
+  const router = useRouter();
+  const { data: isLoggedIn, isLoading, isError, error } = useGetLoginStatus();
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return (
+      <div className="flex min-h-screen items-center justify-between p-24">
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+
+  if (isLoggedIn) {
+    router.push("/dashboard");
+    return;
+  }
+
   return (
-    <main className="flex min-h-screen items-center justify-between p-24">
-      <p>Welcome to the Goldman Portfolio Application</p>
-      {/* {children} */}
-    </main>
+    <>
+      <Login />
+    </>
+    // {children}
   );
 }
