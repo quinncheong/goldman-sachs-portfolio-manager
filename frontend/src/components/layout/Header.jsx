83f325d88fb3873d5b3043d9c55049e464e0b36d
Quinn Cheong <quinncheong@hotmail.com>
2023-10-15 14:28:40 +0800

    Change global design system to match figma

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
   }
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-secondary-100">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors : {
-        "primary": "#7399C6",
-        "secondary": "#186ADE",
-        "accent": "#0D4EA6",
-        "neutral": "#11294D",
-        "dark": "#23254D",
-      }
+      colors: {
+        "primary-100": "#7399C6",
+        "primary-200": "#186ADE",
+        "primary-300": "#0D4EA6",
+        "secondary-100": "#11294D",
+        "secondary-200": "#23254D",
+        background: "#C1CCD6",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
+};
