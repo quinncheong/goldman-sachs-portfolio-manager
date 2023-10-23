3f1d6130a5b78c8ba3f5e4ba25958c53e5409e67
japhethleongyh <96232086+japhethleongyh@users.noreply.github.com>
2023-10-23 15:29:23 +0800

    Updated porfolioCalculator to output postitions ratiooutput include the portfolio

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculatorService.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculatorService.java
@@ -5,40 +5,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.is442g1t4.gpa.portfolio.repository.PortfolioRepository;
+import com.is442g1t4.gpa.portfolio.service.PortfolioService;
+import com.is442g1t4.gpa.stock.StockService;
 import com.is442g1t4.gpa.user.User;
 import com.is442g1t4.gpa.user.UserRepository;
 import com.is442g1t4.gpa.portfolio.model.Portfolio;
+import com.is442g1t4.gpa.stock.model.Stock;
 import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStock;
 import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStockService;
 import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStockRepository;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.*;
-import java.util.Optional;
 
 @Service
 public class PortfolioCalculatorService {
     @Autowired
-    private PortfolioRepository portfolioRepository;
+    private PortfolioService portfolioService;
 
-        public List<AllocatedStock> getAllAllocatedStocksInPortfolio(ObjectId id) {
-
-            Portfolio retrievedPortfolio = portfolioRepository.findPortfolioById(id);
-            List<AllocatedStock> allocatedStocks = retrievedPortfolio.getAllocatedStocks();
-            return allocatedStocks;
-        }
+    @Autowired
+    private StockService stockService;
 
         public Map<String, PortfolioCalculator> getCalculatedStockInPortfolio(ObjectId id){
             Map<String, PortfolioCalculator> calculatedStock = new HashMap<>();
+            double totalValue = 0;
 
-            List<AllocatedStock> allocatedStocks = getAllAllocatedStocksInPortfolio(id);
+            List<AllocatedStock> allocatedStocks = portfolioService.getAllAllocatedStocksInPortfolio(id);
 
             for (AllocatedStock allocatedStock : allocatedStocks){
                 String stockTicker = allocatedStock.getStockTicker();
 
                 if (!calculatedStock.containsKey(stockTicker)){
-                    PortfolioCalculator portfolioCalculator = new PortfolioCalculator(stockTicker, allocatedStock.getStockQuantity(), allocatedStock.getStockBuyPrice());
+                    PortfolioCalculator portfolioCalculator = new PortfolioCalculator(stockTicker, allocatedStock.getStockQuantity(), allocatedStock.getStockBuyPrice(), 0.0, 0.0);
                     calculatedStock.put(stockTicker, portfolioCalculator);
                 }else{
                     PortfolioCalculator portfolioCalculator = calculatedStock.get(stockTicker);
@@ -53,6 +50,17 @@ public class PortfolioCalculatorService {
                     portfolioCalculator.setAvgStockBuyPrice(avgStockBuyPrice);
                     calculatedStock.put(stockTicker, portfolioCalculator);
                 }
+            }
+            for (String stockTicker : calculatedStock.keySet()){
+                PortfolioCalculator portfolioCalculator = calculatedStock.get(stockTicker);
+                Stock stock = stockService.getStockByTicker(stockTicker).get();
+                double value = portfolioCalculator.getStockQuantity() * stock.priceToday();
+                totalValue = totalValue + value;
+                portfolioCalculator.setCurrentValue(value);
+            }
+            for (String stockTicker : calculatedStock.keySet()){
+                PortfolioCalculator portfolioCalculator = calculatedStock.get(stockTicker);
+                portfolioCalculator.setWeight(portfolioCalculator.getCurrentValue()/totalValue);
             }
             return calculatedStock;
         }
