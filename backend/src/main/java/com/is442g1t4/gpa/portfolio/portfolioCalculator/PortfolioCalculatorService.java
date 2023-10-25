5ec9db4d1f5405ab71f81d2f2c44f2ab44b2e5cc
ekcm <86366443+ekcm@users.noreply.github.com>
2023-10-25 12:55:43 +0800

    small fix on portfolioCalculatorService

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculatorService.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculatorService.java
@@ -48,6 +48,7 @@ public class PortfolioCalculatorService {
             for (String stockTicker : calculatedStock.keySet()){
                 PortfolioCalculator portfolioCalculator = calculatedStock.get(stockTicker);
                 // Stock stock = stockService.getStockByTicker(stockTicker).get();
+                // double value = portfolioCalculator.getStockQuantity() * stock.getPriceToday();
                 double value = portfolioCalculator.getStockQuantity() * stockService.getPriceTodayByTicker(stockTicker);
                 portfolioCalculator.setAvgStockBuyPrice(portfolioCalculator.getAvgStockBuyPrice() / portfolioCalculator.getStockQuantity());
                 totalValue = totalValue + value;
