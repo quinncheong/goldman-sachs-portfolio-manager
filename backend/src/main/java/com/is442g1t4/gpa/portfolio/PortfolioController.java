5ba5322ff54f41b8bba70edf2258d51dd3a175eb
Quinn Cheong <quinncheong@hotmail.com>
2023-11-01 17:18:36 +0800

    Hotfix to fix deployment failure

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
@@ -24,9 +24,6 @@ public class PortfolioController {
     @Autowired
     private PortfolioService portfolioService;
 
-    @Autowired
-    private AllocatedStockService allocatedStockService;
-
     @GetMapping("/all")
     public ResponseEntity<List<Portfolio>> getAllPortfolios() {
         return new ResponseEntity<List<Portfolio>>(portfolioService.getAllPortfolios(),
@@ -90,7 +87,8 @@ public class PortfolioController {
     }
 
     @PostMapping("/addAllocatedStock/{portfolioId}/{userId}")
-    public ResponseEntity<Portfolio> createAllocatedStock(@RequestBody AllocatedStock allocatedStock, @PathVariable ObjectId portfolioId, @PathVariable ObjectId userId) {
+    public ResponseEntity<Portfolio> createAllocatedStock(@RequestBody AllocatedStock allocatedStock,
+            @PathVariable ObjectId portfolioId, @PathVariable ObjectId userId) {
         Portfolio updatedPortfolio = portfolioService.addAllocatedStock(allocatedStock, portfolioId, userId);
 
         if (updatedPortfolio == null) {
