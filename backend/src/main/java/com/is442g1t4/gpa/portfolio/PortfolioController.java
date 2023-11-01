7c364b0575fb8ca0eca2a73e9843561279533ada
japhethleongyh <96232086+japhethleongyh@users.noreply.github.com>
2023-11-01 16:58:52 +0800

    I realised Elijah needs that line

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
@@ -17,12 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStock;
+import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStockService;
 
 @RestController
 @RequestMapping("/api/v1/portfolio")
 public class PortfolioController {
     @Autowired
     private PortfolioService portfolioService;
+
+    @Autowired
+    private AllocatedStockService allocatedStockService;
 
     @GetMapping("/all")
     public ResponseEntity<List<Portfolio>> getAllPortfolios() {
