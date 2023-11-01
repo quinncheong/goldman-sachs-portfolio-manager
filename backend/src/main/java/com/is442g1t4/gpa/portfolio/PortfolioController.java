c7324443c7380cbca0d0d94cac443b0718be5af9
Elijah Khor <86366443+ekcm@users.noreply.github.com>
2023-11-01 11:46:42 +0800

    Update PortfolioController.java

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/PortfolioController.java
@@ -3,8 +3,6 @@ package com.is442g1t4.gpa.portfolio;
 import java.util.List;
 import java.util.Optional;
 
-import javax.sound.sampled.Port;
-
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
