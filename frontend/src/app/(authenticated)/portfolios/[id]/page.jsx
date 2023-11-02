0a17e82d8171ace7c2bf55ed43780fa001d96a45
Chester Chia <chester.chia@gmail.com>
2023-11-02 17:07:46 +0800

    Update portfolio

--- frontend/src/app/(authenticated)/portfolios/[id]/page.jsx
+++ frontend/src/app/(authenticated)/portfolios/[id]/page.jsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import Loader from "@/components/loading/Loader";
 import { useRouter } from "next/navigation";
-import {
-  useDeletePortfolio,
-  useGetPortfolioByPortfolioId,
-} from "@/api/portfolio";
+import { useGetPortfolioByPortfolioId, useUpdatePortfolio } from "@/api/portfolio";
 import PortfolioFinancials from "./PortfolioFinancials";
 import PortfolioAnalysis from "./PortfolioAnalysis";
 import StockHoldings from "./StockHoldings";
-import AddStockModal from "./AddStockModal";
 
 export default function PortfolioPage({ params }) {
   const router = useRouter();
@@ -18,21 +13,18 @@ export default function PortfolioPage({ params }) {
     params.id
   );
 
-  function openModal() {
-    document.getElementById("add-stock-modal").showModal();
+  const { isCreating, isSuccessCreating, isErrorCreating, error: updateError, mutate } = useUpdatePortfolio();
+
+  const handleBlur = async (e) => {
+    const updated = { ...data, name: e.target.innerText }
+    await mutate(updated)
+  };
+
+  const handleDescBlur = async (e) => {
+    const updated = { ...data, description: e.target.innerText }
+    await mutate(updated)
   }
 
-  function closeModal() {
-    document.getElementById("add-stock-modal").close();
-  }
-  const {
-    isDeleteing,
-    isSuccessDeleting,
-    isErrorDeleting,
-    deleteError,
-    delPortfolio,
-  } = useDeletePortfolio();
-  console.log(data);
 
   if (isLoading) {
     return <Loader />;
@@ -46,16 +38,6 @@ export default function PortfolioPage({ params }) {
     );
   }
 
-  const editPorfolio = (e) => {
-    e.preventDefault();
-  };
-
-  const handleDeletePortfolio = (e) => {
-    e.preventDefault();
-    delPortfolio(params.id);
-    router.push("/portfolios");
-  };
-
   const addStock = (e) => {
     e.preventDefault();
     router.push("/addstock");
@@ -64,25 +46,15 @@ export default function PortfolioPage({ params }) {
   return (
     <div className="container mx-auto p-4 text-black">
       <div className="flex flex-row justify-between items-center">
-        <div className="containeer p-4 text-black">
-          <h2 className="text-4xl font-semibold">{data.name}</h2>
-          <p className="text-xl">{data.description}</p>
+        <div className="container p-4 text-black">
+          <h2 className="text-4xl font-semibold border-none outline-none" contentEditable="true" suppressContentEditableWarning={true} onBlur={handleBlur}>
+            {data.name}
+          </h2>
+          <p className="text-xl border-none outline-none" contentEditable="true" suppressContentEditableWarning={true} onBlur={handleDescBlur}>{data.description}</p>
         </div>
         <button
-          className="btn btn-error p-4 text-white border-0"
-          onClick={handleDeletePortfolio}
-        >
-          Delete Portfolio
-        </button>
-        <button
-          className="btn btn-info p-4 text-white border-0"
-          onClick={editPorfolio}
-        >
-          Edit Portfolio
-        </button>
-        <button
           className="btn bg-primary-200 p-4 text-white border-0"
-          onClick={openModal}
+          onClick={addStock}
         >
           Add Stocks
         </button>
@@ -98,12 +70,6 @@ export default function PortfolioPage({ params }) {
       </div>
       {/* <PortfolioAnalysis /> */}
       <StockHoldings />
-
-      <AddStockModal
-        portfolio={data}
-        openModal={openModal}
-        closeModal={closeModal}
-      />
     </div>
   );
 }
