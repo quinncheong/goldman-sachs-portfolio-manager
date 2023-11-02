16f4c3c49473c398b91f4f395c9aa7460e97ec36
Quinn Cheong <quinncheong@hotmail.com>
2023-11-02 17:16:10 +0800

    Add ispublic on backend

--- backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculator.java
+++ backend/src/main/java/com/is442g1t4/gpa/portfolio/portfolioCalculator/PortfolioCalculator.java
@@ -1,13 +1,5 @@
 package com.is442g1t4.gpa.portfolio.portfolioCalculator;
 
-import com.is442g1t4.gpa.portfolio.allocatedStock.AllocatedStock;
-
-import java.util.List;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PortfolioCalculator {
-    
+
     private String stockTicker;
     private int stockQuantity;
     private double avgStockBuyPrice;
