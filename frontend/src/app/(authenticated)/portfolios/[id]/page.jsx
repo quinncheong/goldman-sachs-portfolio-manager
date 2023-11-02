7e4adc001a0c94b84d81062820bbf83ba94fbaff
Quinn Cheong <quinncheong@hotmail.com>
2023-11-02 18:08:28 +0800

    Add modification controls for public portfolios

--- frontend/src/app/(authenticated)/portfolios/[id]/page.jsx
+++ frontend/src/app/(authenticated)/portfolios/[id]/page.jsx
@@ -13,12 +13,17 @@ import PortfolioFinancials from "./PortfolioFinancials";
 import PortfolioAnalysis from "./PortfolioAnalysis";
 import StockHoldings from "./StockHoldings";
 import AddStockModal from "./AddStockModal";
+import { getCookie } from "cookies-next";
+import { jwtDecode } from "jwt-decode";
 
 export default function PortfolioPage({ params }) {
   const router = useRouter();
-  const { data, isLoading, isError, error } = useGetPortfolioByPortfolioId(
-    params.id
-  );
+  const {
+    data: portfolio,
+    isLoading,
+    isError,
+    error,
+  } = useGetPortfolioByPortfolioId(params.id);
 
   function openModal() {
     document.getElementById("add-stock-modal").showModal();
@@ -34,7 +39,6 @@ export default function PortfolioPage({ params }) {
     deleteError,
     delPortfolio,
   } = useDeletePortfolio();
-  console.log(data);
 
   if (isLoading) {
     return <Loader />;
@@ -67,13 +71,43 @@ export default function PortfolioPage({ params }) {
     <div className="container mx-auto p-4 text-black">
       <div className="flex flex-row justify-between items-center">
         <div className="container p-4 text-black">
-          <h2 className="text-4xl mt-2 font-semibold">{data.name}</h2>
-          <p className="text-xl mt-2">{data.description}</p>
+          <h2 className="text-4xl mt-2 font-semibold">{portfolio.name}</h2>
+          <p className="text-xl mt-2">{portfolio.description}</p>
           <p className="text-xl mt-2">
             This Porfolio is:{" "}
-            <IsPublicBadge isPublic={data.publiclyAccessible || false} />
+            <IsPublicBadge isPublic={portfolio.publiclyAccessible || false} />
           </p>
         </div>
+        <RenderButtonsWithAccessControl portfolioUserId={portfolio.userId} />
+      </div>
+
+      <div className="rounded-md p-4 text-white bg-secondary-100">
+        <h2 className="text-2xl font-semibold">Portfolio Analysis</h2>
+      </div>
+      <PortfolioAnalysis />
+
+      <div className="rounded-md p-4 text-white bg-secondary-100">
+        <h2 className="text-2xl font-semibold">Holdings</h2>
+      </div>
+      {/* <PortfolioAnalysis /> */}
+      <StockHoldings />
+
+      <AddStockModal
+        portfolio={portfolio}
+        openModal={openModal}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+
+  function RenderButtonsWithAccessControl({ portfolioUserId }) {
+    let userId = jwtDecode(getCookie("token")).userId;
+    if (!(userId === portfolioUserId)) {
+      return;
+    }
+
+    return (
+      <>
         <button
           className="btn btn-error p-4 text-white border-0"
           onClick={handleDeletePortfolio}
@@ -92,24 +126,7 @@ export default function PortfolioPage({ params }) {
         >
           Add Stocks
         </button>
-      </div>
-
-      <div className="rounded-md p-4 text-white bg-secondary-100">
-        <h2 className="text-2xl font-semibold">Portfolio Analysis</h2>
-      </div>
-      <PortfolioAnalysis />
-
-      <div className="rounded-md p-4 text-white bg-secondary-100">
-        <h2 className="text-2xl font-semibold">Holdings</h2>
-      </div>
-      {/* <PortfolioAnalysis /> */}
-      <StockHoldings />
-
-      <AddStockModal
-        portfolio={data}
-        openModal={openModal}
-        closeModal={closeModal}
-      />
-    </div>
-  );
+      </>
+    );
+  }
 }
